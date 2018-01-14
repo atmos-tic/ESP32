0cc2f3c3df9d80ff249cadf73f53f117ca413569
atmos-tic <hantai8118@gmail.com>
2018-01-15 00:04:15 +0900

    [add] ble program

---
+++ ble/BLE_server_test/BLE_server_test.ino
@@ -0,0 +1,46 @@
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "0296b364-7a14-4865-90b4-1c92810e15c5"
+#define CHARACTERISTIC_UUID "5c71f988-e7f7-11e7-80c1-9a214cf093ae"
+int ledpin = 13;
+void setup() {
+  pinMode(ledpin, OUTPUT);
+  digitalWrite(ledpin, HIGH);
+
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+
+  BLEDevice::init("MyESP32");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("Merry Christmas");
+  pService->start();
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledpin, HIGH);
+  delay(100);
+  digitalWrite(ledpin, LOW);
+  delay(100);
+}
